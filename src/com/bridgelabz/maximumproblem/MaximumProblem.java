5ae77aae15c76c1ae637a31cf5424d471fe3079f
namancg <naman.c@perfios.com>
2021-08-30 01:34:39 +0530

    REFACTORED: finding maximum using generic class

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,15 +1,23 @@
 package com.bridgelabz.maximumproblem;
-public class MaximumProblem{
+public class MaximumProblem<E extends Comparable<E>>{
+	E element1, element2, element3;
+	public MaximumProblem(E element1, E element2, E element3) {
+		this.element1 = element1;
+		this.element2 = element2;
+		this.element3 = element3;
+	}
+	public void findMax() {
+		MaximumProblem.findMax(element1, element2, element3);
+	}
+	
 	public static void main(String[] args) {
 		
-		Integer integer1=-88, integer2=33, integer3=-53;
+		Integer integer1=89, integer2=-33, integer3=-53;
 		findMax(integer1,integer2,integer3);
-		
 		Float number1=10.9f, number2=-3.55f, number3=1.88f;
 		findMax(number1,number2,number3);
-		
 		String string1="hello", string2="hello1", string3="hello2";
-		findMax(string1,string2,string3);
+		findMax(string1,string2,string3);	
 	}
 public static <E extends Comparable<E>> void findMax(E element1, E element2, E element3) {
 	E maximum = element1;
@@ -22,5 +30,4 @@ public static <E extends Comparable<E>> void findMax(E element1, E element2, E e
 	System.out.println("The Maximum of "+element1+""+element2+""+element3+" is: "+maximum);
 	
 }
-
 }
