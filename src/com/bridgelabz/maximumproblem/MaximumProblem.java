756b7ef09dbe0622fa2afbad07ae4805a445c0c9
namancg <naman.c@perfios.com>
2021-08-30 00:38:52 +0530

    ADDED: find maximum in 3 string

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -2,13 +2,13 @@ package com.bridgelabz.maximumproblem;
 
 public class MaximumProblem {
 	public static void main(String args[]) {
-	Float float1=30.98f, float2=88.86f, float3=-54f, max;
-	max = float1;
-	if(float2.compareTo(max) > 0)
-		max=float2;
-	if(float3.compareTo(max) > 0)
-		max=float3;
+	String string1="hello", string2="hello1", string3="hello2", max;
+	max = string1;
+	if(string2.compareTo(max) > 0)
+		max=string2;
+	if(string3.compareTo(max) > 0)
+		max=string3;
 	
-	System.out.println("The max of \n"+float1+",\n "+float2+" &\n "+float3+" is: "+max);
+	System.out.println("The max of \n"+string1+",\n "+string2+" &\n "+string3+" is: "+max);
 }
 }
